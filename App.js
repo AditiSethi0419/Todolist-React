8af6936ea5b778cb1ce6fe2057a3e28b40ac61cf
AditiSethi0419 <sethiaditi055@gmail.com>
2025-04-21 16:05:47 +0530

    fixed wrong directory bug

--- App.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from 'react-dom/client';
-
-// create element takes 3 parameters the type of the tag , properties of the tag such as class/id , and the content
-// const heading = React.createElement("h1" , {id: 'heading'} , "Hello World Using React");
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(heading);
